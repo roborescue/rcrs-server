d197d92dd4d840666239b24d395f51a1e3e000a1
Cameron Skinner <camskinner@gmail.com>
2009-04-28 15:06:14 +0100

    Changed sampleagent.sh to use the rescuecore jar

--- boot/sampleagent.sh
+++ boot/sampleagent.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-DIR=`dirname $0`/../build
+DIR=`dirname $0`/../jars/rescuecore.jar
 java -Xmx256m -cp $DIR/ rescuecore.Launch localhost 7000 "0 rescuecore.sample.SampleFireBrigade" "0 rescuecore.sample.SamplePoliceForce" "0 rescuecore.sample.SampleAmbulanceTeam" "0 rescuecore.DummyAgent"
