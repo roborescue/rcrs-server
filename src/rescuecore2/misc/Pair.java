d8f7a63b51355519ce13b1b353aa2d1169a692bf
Cameron Skinner <cam@lappy.(none)>
2009-04-30 16:34:43 +0100

    Added rescuecore2.misc.Pair class

---
+++ src/rescuecore2/misc/Pair.java
@@ -0,0 +1,35 @@
+package rescuecore2.misc;
+
+/**
+   Utility class for a 2-tuple, often known as a pair.
+ */
+public class Pair<S, T> {
+    private S first;
+    private T second;
+
+    /**
+       Construct a pair object.
+       @param first The first element.
+       @param second The second element.
+     */
+    public Pair(S first, T second) {
+	this.first = first;
+	this.second = second;
+    }
+
+    /**
+       Get the first element of this pair.
+       @return The first element.
+     */
+    public S first() {
+	return first;
+    }
+
+    /**
+       Get the second element of this pair.
+       @return The second element.
+     */
+    public T second() {
+	return second;
+    }
+}
