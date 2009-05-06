6b9041f77e7ac5bb33f62ceee0d40a079cff7c75
Cameron Skinner <camskinner@gmail.com>
2009-05-06 14:05:25 +0100

    Rescuecore2: Made WorldModel and WorldModelListener parameterized on a subclass of Entity.
    Also added various useful things to WorldModel such as making it implement Iterable and looking up entities by ID.

--- src/rescuecore2/worldmodel/WorldModel.java
+++ src/rescuecore2/worldmodel/WorldModel.java
@@ -2,29 +2,32 @@ package rescuecore2.worldmodel;
 
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 
 /**
-   This class encapsulates everything about a world model.
- */
-public class WorldModel {
-    private Set<Entity> entities;
-    private Set<WorldModelListener> listeners;
+   This class encapsulates everything about a world model. The world model can be parameterised on a subclass of Entity if required.
+*/
+public class WorldModel<T extends Entity> implements Iterable<T> {
+    private Map<EntityID, T> entities;
+    private Set<WorldModelListener<? super T>> listeners;
 
     /**
        Construct an empty world model.
-     */
+    */
     public WorldModel() {
-        entities = new HashSet<Entity>();
-        listeners = new HashSet<WorldModelListener>();
+        entities = new HashMap<EntityID, T>();
+        listeners = new HashSet<WorldModelListener<? super T>>();
     }
 
     /**
        Add a listener for world model events.
        @param l The listener to add.
-     */
-    public void addWorldModelListener(WorldModelListener l) {
+    */
+    public void addWorldModelListener(WorldModelListener<? super T> l) {
         synchronized (listeners) {
             listeners.add(l);
         }
@@ -33,8 +36,8 @@ public class WorldModel {
     /**
        Remove a listener for world model events.
        @param l The listener to remove.
-     */
-    public void removeWorldModelListener(WorldModelListener l) {
+    */
+    public void removeWorldModelListener(WorldModelListener<? super T> l) {
         synchronized (listeners) {
             listeners.remove(l);
         }
@@ -43,69 +46,91 @@ public class WorldModel {
     /**
        Get all entities in the world.
        @return An immutable view of all entities in the world.
-     */
-    public Set<Entity> getAllEntities() {
-        return Collections.unmodifiableSet(entities);
+    */
+    public Collection<T> getAllEntities() {
+        return Collections.unmodifiableCollection(entities.values());
     }
 
     /**
        Add an entity to the world.
        @param e The entity to add.
-     */
-    public void addEntity(Entity e) {
-        entities.add(e);
+    */
+    public void addEntity(T e) {
+        entities.put(e.getID(), e);
         fireEntityAdded(e);
     }
 
     /**
        Add a set of entities to the world.
        @param e The entities to add.
-     */
-    public void addEntities(Collection<Entity> e) {
-        entities.addAll(e);
-        for (Entity next : e) {
-            fireEntityAdded(next);
+    */
+    public void addEntities(Collection<? extends T> e) {
+        for (T next : e) {
+            addEntity(next);
         }
     }
 
     /**
        Remove an entity from the world.
        @param e The entity to remove.
-     */
-    public void removeEntity(Entity e) {
-        entities.remove(e);
-        fireEntityRemoved(e);
-    }
-
-    /**
-       Remove all entities from the world.
-     */
-    public void removeAllEntities() {
-        Set<Entity> copy = new HashSet<Entity>(entities);
-        entities.clear();
-        for (Entity e : copy) {
+    */
+    public void removeEntity(T e) {
+        if (entities.remove(e.getID()) != null) {
             fireEntityRemoved(e);
         }
     }
 
-    private void fireEntityAdded(Entity e) {
-        for (WorldModelListener l : getListeners()) {
+    /**
+       Remove an entity from the world.
+       @param id The entityID to remove.
+    */
+    public void removeEntity(EntityID id) {
+        T removed = entities.remove(id);
+        if (removed != null) {
+            fireEntityRemoved(removed);
+        }
+    }
+
+    /**
+       Remove all entities from the world.
+    */
+    public void removeAllEntities() {
+        Set<T> all = new HashSet<T>(entities.values());
+        entities.clear();
+        for (T next : all) {
+            fireEntityRemoved(next);
+        }
+    }
+
+    /**
+       Look up an entity by ID.
+       @param id The EntityID to look up.
+       @return The entity with the given ID, or null if no such entity exists.
+     */
+    public T getEntity(EntityID id) {
+        return entities.get(id);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return entities.values().iterator();
+    }
+
+    private void fireEntityAdded(T e) {
+        for (WorldModelListener<? super T> l : getListeners()) {
             l.entityAdded(e);
         }
     }
 
-    private void fireEntityRemoved(Entity e) {
-        for (WorldModelListener l : getListeners()) {
+    private void fireEntityRemoved(T e) {
+        for (WorldModelListener<? super T> l : getListeners()) {
             l.entityRemoved(e);
         }
     }
 
-    private WorldModelListener[] getListeners() {
-        WorldModelListener[] l;
+    private Collection<WorldModelListener<? super T>> getListeners() {
         synchronized (listeners) {
-            l = new WorldModelListener[listeners.size()];
-            listeners.toArray(l);
+            return new HashSet<WorldModelListener<? super T>>(listeners);
         }
-        return l;
     }
 }
--- src/rescuecore2/worldmodel/WorldModelListener.java
+++ src/rescuecore2/worldmodel/WorldModelListener.java
@@ -3,16 +3,16 @@ package rescuecore2.worldmodel;
 /**
    Interface for objects that are interested in changes to the world model.
  */
-public interface WorldModelListener {
+public interface WorldModelListener<T extends Entity> {
     /**
        Notification that an Entity was added to the world.
        @param e The entity that was added.
      */
-    void entityAdded(Entity e);
+    void entityAdded(T e);
 
     /**
        Notification that an Entity was removed from the world.
        @param e The entity that was removed.
      */
-    void entityRemoved(Entity e);
+    void entityRemoved(T e);
 }
