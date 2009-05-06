a6f90790fa38c5cb97ce3e7793d8c4e25d092137
Cameron Skinner <camskinner@gmail.com>
2009-05-06 16:43:53 +0100

    Rescuecore2: Fixed VKAcknowledge message structure

--- src/rescuecore2/version0/messages/VKAcknowledge.java
+++ src/rescuecore2/version0/messages/VKAcknowledge.java
@@ -6,31 +6,10 @@ import rescuecore2.messages.AbstractMessage;
    A message for acknowleding a connection to the kernel.
  */
 public class VKAcknowledge extends AbstractMessage {
-    private IntComponent id;
-
     /**
        VKAcknowledge message with an undefined ID component.
      */
     public VKAcknowledge() {
         super("VK_ACKNOWLEDGE", MessageConstants.VK_ACKNOWLEDGE);
-        id = new IntComponent("ID");
-        addMessageComponent(id);
-    }
-
-    /**
-       VKAcknowledge message with a specific ID component.
-       @param id The value of the ID component.
-     */
-    public VKAcknowledge(int id) {
-        this();
-        this.id.setValue(id);
-    }
-
-    /**
-       Get the ID of the viewer that is acknowledging the connection.
-       @return The viewer ID component.
-     */
-    public int getViewerID() {
-        return id.getValue();
     }
 }
