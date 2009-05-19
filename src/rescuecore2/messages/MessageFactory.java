fb1b83110c056fb8ff2e66d0fcd4fc11aabe57f7
Cameron Skinner <camskinner@gmail.com>
2009-05-19 17:41:01 +0100

    Rescuecore2: Updated javadoc for MessageFactory interface.

--- src/rescuecore2/messages/MessageFactory.java
+++ src/rescuecore2/messages/MessageFactory.java
@@ -15,10 +15,10 @@ public interface MessageFactory {
     //    Message createMessage(int id);
 
     /**
-       Create a message based on its type ID and populate it with data from a stream.
+       Create a message based on its type ID and populate it with data from a stream. If the ID is not recognised then return null.
        @param id The id of the message type to create.
        @param data An InputStream to read message data from.
-       @return A new Message object.
+       @return A new Message object, or null if the ID is not recognised.
        @throws IOException If there is a problem reading the stream.
      */
     Message createMessage(int id, InputStream data) throws IOException;
