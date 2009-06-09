38e85b1e29904d28a40337190f08455a2ddd2337
Cameron Skinner <camskinner@gmail.com>
2009-06-09 16:09:44 +0100

    Updated sampleagent startup script.

--- boot/sampleagent.sh
+++ boot/sampleagent.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 DIR=`dirname $0`/../src/
-java -Xmx256m -cp $DIR/ rescuecore.Launch localhost 7000 "0 rescuecore.sample.SampleFireBrigade" "0 rescuecore.sample.SamplePoliceForce" "0 rescuecore.sample.SampleAmbulanceTeam" "0 rescuecore.DummyAgent"
+java -Xmx256m -cp $DIR/ rescuecore.Launch localhost 7000 "0 rescuecore.sample.SampleFireBrigade" "0 rescuecore.sample.SamplePoliceForce" "0 rescuecore.sample.SampleAmbulanceTeam" "0 rescuecore.sample.SampleCenter"
