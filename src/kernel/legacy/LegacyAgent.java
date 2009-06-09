99e84252d534c19a280d90a1e881fcf0d69c9df4
Cameron Skinner <camskinner@gmail.com>
2009-06-09 16:15:48 +0100

    Kernel: Removed references to old version0 KASense message.

--- src/kernel/legacy/LegacyAgent.java
+++ src/kernel/legacy/LegacyAgent.java
@@ -9,10 +9,9 @@ import kernel.AbstractAgent;
 import rescuecore2.connection.Connection;
 import rescuecore2.messages.Command;
 import rescuecore2.messages.Message;
+import rescuecore2.messages.control.KASense;
 import rescuecore2.worldmodel.Entity;
 
-import rescuecore2.version0.entities.RescueEntity;
-import rescuecore2.version0.messages.KASense;
 import rescuecore2.version0.messages.AgentCommand;
 
 /**
@@ -30,13 +29,7 @@ public class LegacyAgent extends AbstractAgent {
 
     @Override
     public void sendPerceptionUpdate(int time, Collection<? extends Entity> visible, Collection<? extends Message> comms) {
-        Collection<RescueEntity> res = new ArrayList<RescueEntity>();
-        for (Entity next : visible) {
-            if (next instanceof RescueEntity) {
-                res.add((RescueEntity)next);
-            }
-        }
-        KASense sense = new KASense(getControlledEntity().getID().getValue(), time, res);
+        KASense sense = new KASense(getControlledEntity().getID(), time, visible);
         Collection<Message> all = new ArrayList<Message>();
         all.add(sense);
         all.addAll(comms);
