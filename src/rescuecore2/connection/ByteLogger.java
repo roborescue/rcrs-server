754dfff8e71939ab645bdc14cea3107a21b863d1
Cameron Skinner <camskinner@gmail.com>
2009-06-09 16:05:21 +0100

    Rescuecore2: Fixed signed byte issue in ByteLogger.

--- src/rescuecore2/connection/ByteLogger.java
+++ src/rescuecore2/connection/ByteLogger.java
@@ -13,15 +13,20 @@ public final class ByteLogger {
      */
     public static void log(byte[] bytes) {
         // CHECKSTYLE:OFF:MagicNumber
-        System.out.println(bytes.length + "-byte message");
-        System.out.println("Offset    Hex         Int");
+        System.err.println(bytes.length + "-byte message");
+        System.err.println("Offset    Hex         Int");
         for (int i = 0; i < bytes.length; i += 4) {
-            System.out.printf("%1$-8d  0x%2$02x%3$02x%4$02x%5$02x  %2$3d %3$3d %4$3d %5$3d%n",
-                              i,
-                              bytes[i],
-                              (i + 1) >= bytes.length ? 0 : bytes[i + 1],
-                              (i + 2) >= bytes.length ? 0 : bytes[i + 2],
-                              (i + 3) >= bytes.length ? 0 : bytes[i + 3]);
+            byte b1 = bytes[i];
+            byte b2 = (i + 1) >= bytes.length ? 0 : bytes[i + 1];
+            byte b3 = (i + 2) >= bytes.length ? 0 : bytes[i + 2];
+            byte b4 = (i + 3) >= bytes.length ? 0 : bytes[i + 3];
+            int v1 = (int)(bytes[i] & 0xFF);
+            int v2 = (i + 1) >= bytes.length ? 0 : (int)(bytes[i + 1] & 0xFF);
+            int v3 = (i + 2) >= bytes.length ? 0 : (int)(bytes[i + 2] & 0xFF);
+            int v4 = (i + 3) >= bytes.length ? 0 : (int)(bytes[i + 3] & 0xFF);
+            System.err.printf("%1$-8d  0x%2$02x%3$02x%4$02x%5$02x  %6$3d %7$3d %8$3d %9$3d%n",
+                              i, b1, b2, b3, b4,
+                              v1, v2, v3, v4);
         }
         // CHECKSTYLE:ON:MagicNumber
     }
