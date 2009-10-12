6245e0eb3e0c5294728a598eb036038ebbcbbb35
Cameron Skinner <camskinner@gmail.com>
2009-10-12 13:33:09 +0100

    Sample: Updated to use rescuecore2.misc.CommandLineOptions

--- modules/sample/src/sample/LaunchSampleAgents.java
+++ modules/sample/src/sample/LaunchSampleAgents.java
@@ -8,6 +8,10 @@ import rescuecore2.connection.Connection;
 import rescuecore2.connection.ConnectionException;
 import rescuecore2.connection.TCPConnection;
 import rescuecore2.worldmodel.EntityRegistry;
+import rescuecore2.misc.CommandLineOptions;
+import rescuecore2.config.Config;
+import rescuecore2.config.ConfigException;
+import rescuecore2.Constants;
 
 import rescuecore2.standard.entities.StandardEntityFactory;
 
@@ -15,11 +19,6 @@ import rescuecore2.standard.entities.StandardEntityFactory;
    Launcher for sample agents. This will launch as many instances of each of the sample agents as possible, all using one connction.
  */
 public final class LaunchSampleAgents {
-    private static final int DEFAULT_KERNEL_PORT = 7000;
-    private static final String DEFAULT_KERNEL_HOST = "localhost";
-
-    private static final String PORT_FLAG = "-p";
-    private static final String HOST_FLAG = "-h";
     private static final String FIRE_BRIGADE_FLAG = "-fb";
     private static final String POLICE_FORCE_FLAG = "-pf";
     private static final String AMBULANCE_TEAM_FLAG = "-at";
@@ -29,42 +28,41 @@ public final class LaunchSampleAgents {
     /**
        Launch 'em!
        @param args The following arguments are understood: -p <port>, -h <hostname>, -fb <fire brigades>, -pf <police forces>, -at <ambulance teams>
-     */
+    */
     public static void main(String[] args) {
-        EntityRegistry.register(StandardEntityFactory.INSTANCE);
-        int port = DEFAULT_KERNEL_PORT;
-        String host = DEFAULT_KERNEL_HOST;
-        int fb = -1;
-        int pf = -1;
-        int at = -1;
-        // CHECKSTYLE:OFF:ModifiedControlVariable
-        for (int i = 0; i < args.length; ++i) {
-            if (args[i].equals(PORT_FLAG)) {
-                port = Integer.parseInt(args[++i]);
-            }
-            else if (args[i].equals(HOST_FLAG)) {
-                host = args[++i];
-            }
-            else if (args[i].equals(FIRE_BRIGADE_FLAG)) {
-                fb = Integer.parseInt(args[++i]);
-            }
-            else if (args[i].equals(POLICE_FORCE_FLAG)) {
-                pf = Integer.parseInt(args[++i]);
-            }
-            else if (args[i].equals(AMBULANCE_TEAM_FLAG)) {
-                at = Integer.parseInt(args[++i]);
-            }
-            else {
-                System.err.println("Unrecognised option: " + args[i]);
-            }
-        }
-        // CHECKSTYLE:ON:ModifiedControlVariable
         try {
+            EntityRegistry.register(StandardEntityFactory.INSTANCE);
+            Config config = new Config();
+            args = CommandLineOptions.processArgs(args, config);
+            int port = config.getIntValue(Constants.KERNEL_PORT_NUMBER, Constants.DEFAULT_KERNEL_PORT_NUMBER);
+            String host = config.getValue(Constants.KERNEL_HOST_NAME, Constants.DEFAULT_KERNEL_HOST_NAME);
+            int fb = -1;
+            int pf = -1;
+            int at = -1;
+            // CHECKSTYLE:OFF:ModifiedControlVariable
+            for (int i = 0; i < args.length; ++i) {
+                if (args[i].equals(FIRE_BRIGADE_FLAG)) {
+                    fb = Integer.parseInt(args[++i]);
+                }
+                else if (args[i].equals(POLICE_FORCE_FLAG)) {
+                    pf = Integer.parseInt(args[++i]);
+                }
+                else if (args[i].equals(AMBULANCE_TEAM_FLAG)) {
+                    at = Integer.parseInt(args[++i]);
+                }
+                else {
+                    System.err.println("Unrecognised option: " + args[i]);
+                }
+            }
+            // CHECKSTYLE:ON:ModifiedControlVariable
             Connection c = new TCPConnection(host, port);
             c.startup();
             connect(c, fb, pf, at);
         }
         catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (ConfigException e) {
             e.printStackTrace();
         }
         catch (ConnectionException e) {
