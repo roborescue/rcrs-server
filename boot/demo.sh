18d857b060b33b368d8d423236e53e0c3695f308
Cameron Skinner <camskinner@gmail.com>
2009-10-19 18:03:40 +0100

    Updated demo.sh so agent process runs inside an xterm

--- boot/demo.sh
+++ boot/demo.sh
@@ -10,7 +10,7 @@ rm -f $LOGDIR/*.log
 startKernel
 startSims
 
-java -Xmx256m -cp $BASEDIR/jars/rescuecore2.jar:$BASEDIR/jars/standard.jar:$BASEDIR/jars/sample.jar sample.LaunchSampleAgents >& $LOGDIR/agents.log &
+xterm -T agents -e "java -Xmx256m -cp $BASEDIR/jars/rescuecore2.jar:$BASEDIR/jars/standard.jar:$BASEDIR/jars/sample.jar sample.LaunchSampleAgents >& $LOGDIR/agents.log" &
 AGENTS=$!
 
 waitFor $LOGDIR/kernel.log "Kernel has shut down" 30
