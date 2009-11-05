5eaee4c4523af7b438ec6e02c68a9c68a51c197d
Cameron Skinner <camskinner@gmail.com>
2009-11-05 17:12:21 +0000

    Fixed agent logging in demo script

--- boot/demo.sh
+++ boot/demo.sh
@@ -10,7 +10,7 @@ rm -f $LOGDIR/*.log
 startKernel
 startSims
 
-xterm -T agents -e "java -Xmx256m -cp $BASEDIR/jars/rescuecore2.jar:$BASEDIR/jars/standard.jar:$BASEDIR/jars/sample.jar sample.LaunchSampleAgents >& $LOGDIR/agents.log" &
+xterm -T agents -e "java -Xmx256m -cp $BASEDIR/jars/rescuecore2.jar:$BASEDIR/jars/standard.jar:$BASEDIR/jars/sample.jar rescuecore2.LaunchComponents sample.SampleFireBrigade*n sample.SampleAmbulanceTeam*n sample.SamplePoliceForce*n sample.SampleCentre*n 2>&1 | tee $LOGDIR/agents.log" &
 AGENTS=$!
 
 waitFor $LOGDIR/kernel.log "Kernel has shut down" 30
