5dfe7714b66d0bda97ca3aeb10bed43240a8bc42
Cameron Skinner <camskinner@gmail.com>
2010-01-31 19:50:14 +0000

    Standard: Made entity/property/message URNs into proper URNs

---
+++ modules/standard/src/rescuecore2/standard/Constants.java
@@ -0,0 +1,16 @@
+package rescuecore2.standard;
+
+/**
+   Constants for the standard package.
+*/
+public final class Constants {
+    /** Prefix for entity URNs. */
+    public static final String ENTITY_URN_PREFIX = "urn:rescuecore2.standard:entity:";
+    /** Prefix for property URNs. */
+    public static final String PROPERTY_URN_PREFIX = "urn:rescuecore2.standard:property:";
+    /** Prefix for message URNs. */
+    public static final String MESSAGE_URN_PREFIX = "urn:rescuecore2.standard:message:";
+
+    private Constants() {
+    }
+}
--- modules/standard/src/rescuecore2/standard/entities/StandardEntityURN.java
+++ modules/standard/src/rescuecore2/standard/entities/StandardEntityURN.java
@@ -1,23 +1,50 @@
 package rescuecore2.standard.entities;
 
+import static rescuecore2.standard.Constants.ENTITY_URN_PREFIX;
+
 /**
    URNs for standard entities.
  */
 public enum StandardEntityURN {
     // CHECKSTYLE:OFF:JavadocVariableCheck
 
-    WORLD,
-    ROAD,
-    BLOCKADE,
-    BUILDING,
-    REFUGE,
-    FIRE_STATION,
-    AMBULANCE_CENTRE,
-    POLICE_OFFICE,
-    CIVILIAN,
-    FIRE_BRIGADE,
-    AMBULANCE_TEAM,
-    POLICE_FORCE;
+    WORLD(ENTITY_URN_PREFIX + "world"),
+    ROAD(ENTITY_URN_PREFIX + "road"),
+    BLOCKADE(ENTITY_URN_PREFIX + "blockade"),
+    BUILDING(ENTITY_URN_PREFIX + "building"),
+    REFUGE(ENTITY_URN_PREFIX + "refuge"),
+    FIRE_STATION(ENTITY_URN_PREFIX + "firestation"),
+    AMBULANCE_CENTRE(ENTITY_URN_PREFIX + "ambulancecentre"),
+    POLICE_OFFICE(ENTITY_URN_PREFIX + "policeoffice"),
+    CIVILIAN(ENTITY_URN_PREFIX + "civilian"),
+    FIRE_BRIGADE(ENTITY_URN_PREFIX + "firebrigade"),
+    AMBULANCE_TEAM(ENTITY_URN_PREFIX + "ambulanceteam"),
+    POLICE_FORCE(ENTITY_URN_PREFIX + "policeforce");
 
     // CHECKSTYLE:ON:JavadocVariableCheck
+
+    private String urn;
+
+    private StandardEntityURN(String urn) {
+        this.urn = urn;
+    }
+
+    @Override
+    public String toString() {
+        return urn;
+    }
+
+    /**
+       Convert a String to a StandardEntityURN.
+       @param s The String to convert.
+       @return A StandardEntityURN.
+    */
+    public static StandardEntityURN fromString(String s) {
+        for (StandardEntityURN next : StandardEntityURN.values()) {
+            if (next.urn.equals(s)) {
+                return next;
+            }
+        }
+        throw new IllegalArgumentException(s);
+    }
 }
