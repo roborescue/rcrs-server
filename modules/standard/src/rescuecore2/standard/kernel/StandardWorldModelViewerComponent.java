ee3d67c2f17c996323681fe9ae89b72fc9492194
Cameron Skinner <camskinner@gmail.com>
2010-02-02 11:51:28 +0000

    Standard: StandardWorldModelViewerComponent can now look up entities by ID

--- modules/standard/src/rescuecore2/standard/kernel/StandardWorldModelViewerComponent.java
+++ modules/standard/src/rescuecore2/standard/kernel/StandardWorldModelViewerComponent.java
@@ -1,9 +1,14 @@
 package rescuecore2.standard.kernel;
 
 import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.JComponent;
 import javax.swing.JSplitPane;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
 
 import java.util.List;
 
@@ -11,7 +16,9 @@ import rescuecore2.view.EntityInspector;
 import rescuecore2.view.ViewListener;
 import rescuecore2.view.ViewComponent;
 import rescuecore2.view.RenderedObject;
+import rescuecore2.worldmodel.WorldModel;
 import rescuecore2.worldmodel.Entity;
+import rescuecore2.worldmodel.EntityID;
 //import rescuecore2.config.Config;
 import rescuecore2.Timestep;
 import rescuecore2.GUIComponent;
@@ -29,7 +36,9 @@ public class StandardWorldModelViewerComponent extends KernelListenerAdapter imp
 
     private StandardWorldModelViewer viewer;
     private EntityInspector inspector;
+    private JTextField field;
     private JComponent view;
+    private WorldModel<? extends Entity> world;
 
     /**
        Construct a StandardWorldModelViewerComponent.
@@ -37,6 +46,7 @@ public class StandardWorldModelViewerComponent extends KernelListenerAdapter imp
     public StandardWorldModelViewerComponent() {
         viewer = new StandardWorldModelViewer();
         inspector = new EntityInspector();
+        field = new JTextField();
         viewer.setPreferredSize(new Dimension(SIZE, SIZE));
         viewer.addViewListener(new ViewListener() {
                 @Override
@@ -44,6 +54,7 @@ public class StandardWorldModelViewerComponent extends KernelListenerAdapter imp
                     for (RenderedObject next : objects) {
                         if (next.getObject() instanceof Entity) {
                             inspector.inspect((Entity)next.getObject());
+                            field.setText("");
                             return;
                         }
                     }
@@ -53,13 +64,31 @@ public class StandardWorldModelViewerComponent extends KernelListenerAdapter imp
                 public void objectsRollover(ViewComponent v, List<RenderedObject> objects) {
                 }
             });
-        view = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, viewer, new JScrollPane(inspector));
+        field.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    String s = field.getText();
+                    try {
+                        int id = Integer.parseInt(s);
+                        EntityID eid = new EntityID(id);
+                        Entity e = world.getEntity(eid);
+                        inspector.inspect(e);
+                    }
+                    catch (NumberFormatException e) {
+                        field.setText("");
+                    }
+                }
+            });
+        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, viewer, new JScrollPane(inspector));
+        view = new JPanel(new BorderLayout());
+        view.add(split, BorderLayout.CENTER);
+        view.add(field, BorderLayout.NORTH);
     }
 
     @Override
     public void simulationStarted(Kernel kernel) {
         viewer.initialise(kernel.getConfig());
-        viewer.view(kernel.getWorldModel());
+        world = kernel.getWorldModel();
+        viewer.view(world);
         viewer.repaint();
     }
 
