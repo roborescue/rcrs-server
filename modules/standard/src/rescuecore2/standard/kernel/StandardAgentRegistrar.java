83074d782b7e2ccf2499c0a583bbfa5054d748b0
Cameron Skinner <camskinner@gmail.com>
2010-02-04 16:22:59 +0000

    Standard: StandardAgentRegistrar now sends correct config options to agents

--- modules/standard/src/rescuecore2/standard/kernel/StandardAgentRegistrar.java
+++ modules/standard/src/rescuecore2/standard/kernel/StandardAgentRegistrar.java
@@ -3,8 +3,11 @@ package rescuecore2.standard.kernel;
 import java.util.Set;
 import java.util.HashSet;
 
+import java.util.regex.PatternSyntaxException;
+
 import kernel.AgentRegistrar;
 import kernel.ComponentManager;
+import kernel.KernelException;
 
 import rescuecore2.config.Config;
 import rescuecore2.worldmodel.WorldModel;
@@ -39,27 +42,41 @@ public class StandardAgentRegistrar implements AgentRegistrar {
     private static final Set<String> VISIBLE_CONFIG_OPTIONS = new HashSet<String>();
 
     static {
-        VISIBLE_CONFIG_OPTIONS.add("kernel.agents.think-time");
-        VISIBLE_CONFIG_OPTIONS.add("kernel.startup.connect-time");
-        VISIBLE_CONFIG_OPTIONS.add("fire.tank.maximum");
-        VISIBLE_CONFIG_OPTIONS.add("fire.tank.refill-rate");
-        VISIBLE_CONFIG_OPTIONS.add("fire.extinguish.max-sum");
-        VISIBLE_CONFIG_OPTIONS.add("fire.extinguish.max-distance");
-        VISIBLE_CONFIG_OPTIONS.add(Constants.COMMUNICATION_MODEL_KEY);
-        VISIBLE_CONFIG_OPTIONS.add(Constants.PERCEPTION_KEY);
-        VISIBLE_CONFIG_OPTIONS.add(StandardConstants.FIRE_BRIGADE_COUNT_KEY);
-        VISIBLE_CONFIG_OPTIONS.add(StandardConstants.FIRE_STATION_COUNT_KEY);
-        VISIBLE_CONFIG_OPTIONS.add(StandardConstants.AMBULANCE_TEAM_COUNT_KEY);
-        VISIBLE_CONFIG_OPTIONS.add(StandardConstants.AMBULANCE_CENTRE_COUNT_KEY);
-        VISIBLE_CONFIG_OPTIONS.add(StandardConstants.POLICE_FORCE_COUNT_KEY);
-        VISIBLE_CONFIG_OPTIONS.add(StandardConstants.POLICE_OFFICE_COUNT_KEY);
+        VISIBLE_CONFIG_OPTIONS.add("kernel\\.agents\\.think-time");
+        VISIBLE_CONFIG_OPTIONS.add("kernel\\.startup\\.connect-time");
+        VISIBLE_CONFIG_OPTIONS.add(Constants.COMMUNICATION_MODEL_KEY.replace(".", "\\."));
+        VISIBLE_CONFIG_OPTIONS.add(Constants.PERCEPTION_KEY.replace(".", "\\."));
+        VISIBLE_CONFIG_OPTIONS.add("fire\\.tank\\.maximum");
+        VISIBLE_CONFIG_OPTIONS.add("fire\\.tank\\.refill-rate");
+        VISIBLE_CONFIG_OPTIONS.add("fire\\.extinguish\\.max-sum");
+        VISIBLE_CONFIG_OPTIONS.add("fire\\.extinguish\\.max-distance");
+        VISIBLE_CONFIG_OPTIONS.add(StandardConstants.FIRE_BRIGADE_COUNT_KEY.replace(".", "\\."));
+        VISIBLE_CONFIG_OPTIONS.add(StandardConstants.FIRE_STATION_COUNT_KEY.replace(".", "\\."));
+        VISIBLE_CONFIG_OPTIONS.add(StandardConstants.AMBULANCE_TEAM_COUNT_KEY.replace(".", "\\."));
+        VISIBLE_CONFIG_OPTIONS.add(StandardConstants.AMBULANCE_CENTRE_COUNT_KEY.replace(".", "\\."));
+        VISIBLE_CONFIG_OPTIONS.add(StandardConstants.POLICE_FORCE_COUNT_KEY.replace(".", "\\."));
+        VISIBLE_CONFIG_OPTIONS.add(StandardConstants.POLICE_OFFICE_COUNT_KEY.replace(".", "\\."));
+        VISIBLE_CONFIG_OPTIONS.add("comms\\.channels\\.count");
+        VISIBLE_CONFIG_OPTIONS.add("comms\\.channels\\.max\\.platoon");
+        VISIBLE_CONFIG_OPTIONS.add("comms\\.channels\\.max\\.centre");
+        VISIBLE_CONFIG_OPTIONS.add("comms\\.channels\\.\\d+\\.type");
+        VISIBLE_CONFIG_OPTIONS.add("comms\\.channels\\.\\d+\\.range");
+        VISIBLE_CONFIG_OPTIONS.add("comms\\.channels\\.\\d+\\.messages\\.size");
+        VISIBLE_CONFIG_OPTIONS.add("comms\\.channels\\.\\d+\\.messages\\.max");
+        VISIBLE_CONFIG_OPTIONS.add("comms\\.channels\\.\\d+\\.bandwidth");
+        VISIBLE_CONFIG_OPTIONS.add("clear\\.repair\\.rate");
     }
 
     @Override
-    public void registerAgents(WorldModel<? extends Entity> world, Config config, ComponentManager manager) {
+    public void registerAgents(WorldModel<? extends Entity> world, Config config, ComponentManager manager) throws KernelException {
         StandardWorldModel model = StandardWorldModel.createStandardWorldModel(world);
         Config agentConfig = new Config(config);
-        agentConfig.removeExcept(VISIBLE_CONFIG_OPTIONS);
+        try {
+            agentConfig.removeExceptRegex(VISIBLE_CONFIG_OPTIONS);
+        }
+        catch (PatternSyntaxException e) {
+            throw new KernelException(e);
+        }
         agentConfig.setIntValue(StandardConstants.FIRE_BRIGADE_COUNT_KEY, model.getEntitiesOfType(StandardEntityURN.FIRE_BRIGADE).size());
         agentConfig.setIntValue(StandardConstants.FIRE_STATION_COUNT_KEY, model.getEntitiesOfType(StandardEntityURN.FIRE_STATION).size());
         agentConfig.setIntValue(StandardConstants.AMBULANCE_TEAM_COUNT_KEY, model.getEntitiesOfType(StandardEntityURN.AMBULANCE_TEAM).size());
