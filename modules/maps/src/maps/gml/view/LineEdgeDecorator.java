c4acb6126d816ec579337a38d6e93e4358371f3d
Cameron Skinner <camskinner@gmail.com>
2010-05-12 15:26:05 +0100

    Maps: LineEdgeDecorator now draws impassable edges thicker than passable ones

--- modules/maps/src/maps/gml/view/LineEdgeDecorator.java
+++ modules/maps/src/maps/gml/view/LineEdgeDecorator.java
@@ -4,11 +4,16 @@ import maps.gml.GMLEdge;
 import rescuecore2.misc.gui.ScreenTransform;
 import java.awt.Graphics2D;
 import java.awt.Color;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
 
 /**
    An EdgeDecorator that draws a line for each edge.
 */
 public class LineEdgeDecorator implements EdgeDecorator {
+    private static final Stroke PASSABLE_STROKE = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
+    private static final Stroke IMPASSABLE_STROKE = new BasicStroke(2, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
+
     private Color colour;
 
     /**
@@ -26,6 +31,7 @@ public class LineEdgeDecorator implements EdgeDecorator {
         int x2 = transform.xToScreen(edge.getEnd().getX());
         int y2 = transform.yToScreen(edge.getEnd().getY());
         g.setColor(colour);
+        g.setStroke(edge.isPassable() ? PASSABLE_STROKE : IMPASSABLE_STROKE);
         g.drawLine(x1, y1, x2, y2);
     }
 }
