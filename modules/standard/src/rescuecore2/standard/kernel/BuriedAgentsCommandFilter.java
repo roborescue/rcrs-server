13c8e724cba922f0af6364767afccf880492ba82
Cameron Skinner <camskinner@gmail.com>
2010-05-13 08:54:16 +0100

    Standard: BuriedAgentsCommandFilter now allows communication commands for buried agents

--- modules/standard/src/rescuecore2/standard/kernel/BuriedAgentsCommandFilter.java
+++ modules/standard/src/rescuecore2/standard/kernel/BuriedAgentsCommandFilter.java
@@ -7,7 +7,11 @@ import rescuecore2.messages.Command;
 import rescuecore2.worldmodel.Entity;
 import rescuecore2.worldmodel.EntityID;
 import rescuecore2.log.Logger;
+
 import rescuecore2.standard.entities.Human;
+import rescuecore2.standard.messages.AKSubscribe;
+import rescuecore2.standard.messages.AKSpeak;
+import rescuecore2.standard.messages.AKSay;
 
 /**
    A CommandFilter that discards commands from buried agents.
@@ -17,10 +21,15 @@ public class BuriedAgentsCommandFilter extends AbstractCommandFilter {
     protected boolean allowed(Command c, KernelState state) {
         EntityID id = c.getAgentID();
         Entity e = state.getWorldModel().getEntity(id);
+        if ((c instanceof AKSubscribe)
+            || (c instanceof AKSpeak)
+            || (c instanceof AKSay)) {
+            return true;
+        }
         if (e instanceof Human) {
             Human h = (Human)e;
             if (h.isBuriednessDefined() && h.getBuriedness() > 0) {
-                Logger.info("Ignoring command from buried agent " + h);
+                Logger.info("Ignoring command " + c + " from buried agent " + h);
                 return false;
             }
         }
