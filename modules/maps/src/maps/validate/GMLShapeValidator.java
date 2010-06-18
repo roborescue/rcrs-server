9dade725921a130785081c2a90f5ed570fc4d0f5
Moritz Goebelbecker <goebelbe@informatik.uni-freiburg.de>
2010-06-18 21:04:44 +0200

    Add check for overlapping shapes.

--- modules/maps/src/maps/validate/GMLShapeValidator.java
+++ modules/maps/src/maps/validate/GMLShapeValidator.java
@@ -18,10 +18,33 @@ public class GMLShapeValidator implements MapValidator<GMLMap> {
     @Override
     public Collection<ValidationError> validate(GMLMap map) {
         List<ValidationError> errors = new ArrayList<ValidationError>();
-        for (GMLShape shape : map.getAllShapes()) {
-            ValidationError error = checkShape(shape);
-            if (error != null) {
-                errors.add(error);
+        List<Geometry> polygons = new ArrayList<Geometry>();
+        List<GMLShape> shapes = new ArrayList<GMLShape>(map.getAllShapes());
+
+        for (GMLShape shape : shapes) {
+            try {
+                Geometry polygon = checkShape(shape);
+                polygons.add(polygon);
+            }
+            catch (ValidationException e) {
+                errors.add(e.getError());
+                polygons.add(null);
+            }
+        }
+
+        for (int i = 0; i < polygons.size(); i++) {
+            Geometry s1 = polygons.get(i);
+            if (s1 == null) {
+                continue;
+            }
+            for (int j = i + 1; j < polygons.size(); j++) {
+                Geometry s2 = polygons.get(j);
+                if (s2 != null && s1.intersects(s2) && !s1.touches(s2)) {
+                    int s1Id = shapes.get(i).getID();
+                    int s2Id = shapes.get(j).getID();
+                    String message = " Shape overlaps with shape " + s2Id;
+                    errors.add(new ValidationError(s1Id, message));
+                }
             }
         }
         return errors;
@@ -32,20 +55,15 @@ public class GMLShapeValidator implements MapValidator<GMLMap> {
        @param shape
        @return
      */
-    private static ValidationError checkShape(GMLShape shape) {
-        try {
-            Geometry polygon = JTSTools.shapeToPolygon(shape);
-            if (!polygon.isValid()) {
-                return new ValidationError(shape.getID(), "invalid shape");
-            }
-            if (!polygon.contains(polygon.getCentroid())) {
-                return new ValidationError(shape.getID(), "Shape doesn't contain centroid.");
-            }
+    private static Geometry checkShape(GMLShape shape) throws ValidationException {
+        Geometry polygon = JTSTools.shapeToPolygon(shape);
+        if (!polygon.isValid()) {
+            throw new ValidationException(shape.getID(), "invalid shape");
         }
-        catch (ValidationException e) {
-            return e.getError();
+        if (!polygon.contains(polygon.getCentroid())) {
+            throw new ValidationException(shape.getID(), "Shape doesn't contain centroid.");
         }
-        return null;
+        return polygon;
     }
 
 }
