5888381ecc285b4a71e25ed4beac6a1c7793b5ad
Moritz Goebelbecker <goebelbe@informatik.uni-freiburg.de>
2012-04-24 22:37:43 +0200

    Add logextract script

---
+++ boot/logextract.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [ ! $# -eq 2 ] ; then
+  echo "usage: `basename $0` <logfile> <outdir>"
+  exit 1
+fi
+
+. functions.sh
+
+makeClasspath $BASEDIR/jars $BASEDIR/lib
+java -Xmx1024m -cp $CP rescuecore2.log.LogExtractor -c config/logviewer.cfg $1 $2
