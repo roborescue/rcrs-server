b135b032d654706e05cc735be0ba0a585893af01
Moritz Göbelbecker <goebelbe@informatik.uni-freiburg.de>
2013-06-25 19:47:07 +0200

    Initial config for robocup 2013

--- scripts/remote-control/config.sh
+++ scripts/remote-control/config.sh
@@ -1,21 +1,21 @@
-LOCAL_USER=goebelbe
+LOCAL_USER=rescue
 REMOTE_USER=rescue
 
-MAPDIR=maps
-KERNELDIR=rescue-1.1-dutchopen2012
+MAPDIR=maps-testing
+KERNELDIR=roborescue-git
 CODEDIR=code
-SCRIPTDIR=scripts
+SCRIPTDIR=$KERNELDIR/scripts/remote-control
 LOGDIR=logs
 DISTDIR=logdist
 EVALDIR=evaluation
 
 RSYNC_OPTS=-C
 
-CLUSTERS="1 2"
+CLUSTERS="1 2 3"
 
-HOSTS="c1-1 c1-2 c1-3 c1-4 c2-1 c2-2 c2-3 c2-4 rescue-control"
-SERVER_HOSTS="c1-1 c2-1"
-CLIENT_HOSTS="c1-2 c1-3 c1-4 c2-2 c2-3 c2-4"
+HOSTS="c1-1 c1-2 c1-3 c1-4 c2-1 c2-2 c2-3 c2-4 c3-1 c3-2 c3-3 c3-4 control"
+SERVER_HOSTS="c1-1 c2-1 c3-1"
+CLIENT_HOSTS="c1-2 c1-3 c1-4 c2-2 c2-3 c2-4 c3-2 c3-3 c3-4"
 
 # HOSTS=localhost
 # SERVER_HOSTS=localhost
@@ -23,14 +23,24 @@ CLIENT_HOSTS="c1-2 c1-3 c1-4 c2-2 c2-3 c2-4"
 
 KERNEL_WAITING_TIME=5
 
-DAY=final
+DAY=testing
 
-TEAM_SHORTHANDS="BAS FCP KAV"
+TEAM_SHORTHANDS="ANC APO CSU GUC LTI MIN MRL NAI POS RI1 RAK SOS ZJU"
 
 declare -A TEAM_NAMES
-TEAM_NAMES[BAS]=Baseline
-TEAM_NAMES[FCP]=FC-Portugal
-TEAM_NAMES[KAV]=Kaveh
+TEAM_NAMES[ANC]=anct_rescue2013
+TEAM_NAMES[APO]=Apollo-Rescue
+TEAM_NAMES[CSU]=CSU-YUNLU
+TEAM_NAMES[GUC]=GUC_ArtSapience
+TEAM_NAMES[LTI]="LTI Agent Rescue"
+TEAM_NAMES[MIN]=MinERS
+TEAM_NAMES[MRL]=MRL
+TEAM_NAMES[NAI]=NAITO-Rescue2013
+TEAM_NAMES[POS]=Poseidon
+TEAM_NAMES[RI1]=Ri-one
+TEAM_NAMES[RAK]=RoboAKUT
+TEAM_NAMES[SOS]=S.O.S.
+TEAM_NAMES[ZJU]=ZJUBase
 
 DIR=$(pwd)
 
