abf53a7e2a7eca1e078217ec20473a17cd409a83
ali <ali@ali-linux>
2017-05-28 16:35:22 +0200

    optimize standard perception

--- boot/start.sh
+++ boot/start.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
-
+trap "echo 'killing...';./kill.sh;exit" INT
 . functions.sh
 
 processArgs $*
 
 # Delete old logs
 rm -f $LOGDIR/*.log
+	sh kill.sh
 
 #startGIS
-startKernel
-startSims
+startKernel --nomenu
+startSims --nogui
 
 echo "Start your agents"
 waitFor $LOGDIR/kernel.log "Kernel has shut down" 30
 
 kill $PIDS
+./kill.sh
