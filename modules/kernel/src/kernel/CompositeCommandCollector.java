6483ef01fcce83d4e8b3d4157ddfc4331e31d776
Okan Asik <asik.okan@gmail.com>
2020-02-20 16:08:08 +0300

    properly cancel the tasks of not returned command collectors

--- modules/kernel/src/kernel/CompositeCommandCollector.java
+++ modules/kernel/src/kernel/CompositeCommandCollector.java
@@ -1,15 +1,20 @@
 package kernel;
 
 import rescuecore2.config.Config;
-import rescuecore2.messages.Command;
 import rescuecore2.log.Logger;
+import rescuecore2.messages.Command;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Set;
 import java.util.HashSet;
 import java.util.Iterator;
-
-import java.util.concurrent.*;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
    A CommandCollector that waits for any of a set of child CommandCollectors to return a result.
@@ -30,15 +35,14 @@ public class CompositeCommandCollector implements CommandCollector {
         for (CommandCollector next : children) {
             next.initialise(config);
         }
+        executorService = Executors.newFixedThreadPool(children.size());
     }
 
     @Override
     public Collection<Command> getAgentCommands(Collection<AgentProxy> agents, int timestep) throws InterruptedException {
+        Collection<Command> result = new ArrayList<Command>();
         if (agents.size() == 0) {
-            return new HashSet<Command>();
-        }
-        if (executorService == null) {
-            executorService = Executors.newFixedThreadPool(children.size());
+            return result;
         }
         ExecutorCompletionService<Collection<Command>> service = new ExecutorCompletionService<Collection<Command>>(executorService);
         Set<Future<Collection<Command>>> futures = new HashSet<Future<Collection<Command>>>();
@@ -46,10 +50,10 @@ public class CompositeCommandCollector implements CommandCollector {
             futures.add(service.submit(new ChildCommandsFetcher(next, agents, timestep)));
         }
         try {
-            int size = children.size();
-            for (int i = 0; i < size; ++i) {
+            for (int i = 0; i < children.size(); ++i) {
                 try {
-                    return service.take().get();
+                    result = service.take().get();
+                    break;
                 }
                 catch (ExecutionException e) {
                     Logger.error("Error while getting agent commands", e);
@@ -61,7 +65,7 @@ public class CompositeCommandCollector implements CommandCollector {
                 next.cancel(true);
             }
         }
-        return new HashSet<Command>();
+        return result;
     }
 
     /**
