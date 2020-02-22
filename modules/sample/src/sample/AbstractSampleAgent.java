bed13d0c9f1b9d974a01d04e4124f5b8a046a4bd
Luis Gustavo Nardin <gnardin@gmail.com>
2020-02-22 20:09:09 +0000

    Fixed SamplePoliceForce

--- modules/sample/src/sample/AbstractSampleAgent.java
+++ modules/sample/src/sample/AbstractSampleAgent.java
@@ -1,120 +1,133 @@
 package sample;
 
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Set;
-import java.util.HashSet;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-
-import rescuecore2.worldmodel.EntityID;
-import rescuecore2.Constants;
+import java.util.Set;
 import org.apache.log4j.Logger;
-
+import rescuecore2.Constants;
 import rescuecore2.standard.components.StandardAgent;
-import rescuecore2.standard.entities.StandardEntity;
 import rescuecore2.standard.entities.Building;
+import rescuecore2.standard.entities.Human;
 import rescuecore2.standard.entities.Refuge;
 import rescuecore2.standard.entities.Road;
-import rescuecore2.standard.entities.Human;
+import rescuecore2.standard.entities.StandardEntity;
 import rescuecore2.standard.kernel.comms.ChannelCommunicationModel;
 import rescuecore2.standard.kernel.comms.StandardCommunicationModel;
+import rescuecore2.worldmodel.EntityID;
 
 /**
-   Abstract base class for sample agents.
-   @param <E> The subclass of StandardEntity this agent wants to control.
+ * Abstract base class for sample agents.
+ *
+ * @param <E>
+ *          The subclass of StandardEntity this agent wants to control.
  */
-public abstract class AbstractSampleAgent<E extends StandardEntity> extends StandardAgent<E> {
-    private static final int RANDOM_WALK_LENGTH = 50;
+public abstract class AbstractSampleAgent<E extends StandardEntity>
+    extends StandardAgent<E> {
 
-    private static final String SAY_COMMUNICATION_MODEL = StandardCommunicationModel.class.getName();
-    private static final String SPEAK_COMMUNICATION_MODEL = ChannelCommunicationModel.class.getName();
-    private static final Logger LOG = Logger.getLogger(AbstractSampleAgent.class);
+  private static final int             RANDOM_WALK_LENGTH        = 50;
 
-    /**
-       The search algorithm.
-    */
-    protected SampleSearch search;
+  private static final String          SAY_COMMUNICATION_MODEL   = StandardCommunicationModel.class
+      .getName();
+  private static final String          SPEAK_COMMUNICATION_MODEL = ChannelCommunicationModel.class
+      .getName();
+  private static final Logger          LOG                       = Logger
+      .getLogger( AbstractSampleAgent.class );
 
-    /**
-       Whether to use AKSpeak messages or not.
-    */
-    protected boolean useSpeak;
+  /**
+   * The search algorithm.
+   */
+  protected SampleSearch               search;
 
-    /**
-       Cache of building IDs.
-    */
-    protected List<EntityID> buildingIDs;
+  /**
+   * Whether to use AKSpeak messages or not.
+   */
+  protected boolean                    useSpeak;
 
-    /**
-       Cache of road IDs.
-    */
-    protected List<EntityID> roadIDs;
+  /**
+   * Cache of building IDs.
+   */
+  protected List<EntityID>             buildingIDs;
 
-    /**
-       Cache of refuge IDs.
-    */
-    protected List<EntityID> refugeIDs;
+  /**
+   * Cache of road IDs.
+   */
+  protected List<EntityID>             roadIDs;
 
-    private Map<EntityID, Set<EntityID>> neighbours;
+  /**
+   * Cache of refuge IDs.
+   */
+  protected List<EntityID>             refugeIDs;
 
-    /**
-       Construct an AbstractSampleAgent.
-    */
-    protected AbstractSampleAgent() {
+  private Map<EntityID, Set<EntityID>> neighbours;
+
+
+  /**
+   * Construct an AbstractSampleAgent.
+   */
+  protected AbstractSampleAgent() {
+  }
+
+
+  @Override
+  protected void postConnect() {
+    super.postConnect();
+    buildingIDs = new ArrayList<EntityID>();
+    roadIDs = new ArrayList<EntityID>();
+    refugeIDs = new ArrayList<EntityID>();
+    for ( StandardEntity next : model ) {
+      if ( next instanceof Building ) {
+        buildingIDs.add( next.getID() );
+      }
+      if ( next instanceof Road ) {
+        roadIDs.add( next.getID() );
+      }
+      if ( next instanceof Refuge ) {
+        refugeIDs.add( next.getID() );
+      }
     }
+    search = new SampleSearch( model );
+    neighbours = search.getGraph();
+    useSpeak = config.getValue( Constants.COMMUNICATION_MODEL_KEY )
+        .equals( SPEAK_COMMUNICATION_MODEL );
+    LOG.debug( "Communcation model: "
+        + config.getValue( Constants.COMMUNICATION_MODEL_KEY ) );
+    LOG.debug( useSpeak ? "Using speak model" : "Using say model" );
+  }
 
-    @Override
-    protected void postConnect() {
-        super.postConnect();
-        buildingIDs = new ArrayList<EntityID>();
-        roadIDs = new ArrayList<EntityID>();
-        refugeIDs = new ArrayList<EntityID>();
-        for (StandardEntity next : model) {
-            if (next instanceof Building) {
-                buildingIDs.add(next.getID());
-            }
-            if (next instanceof Road) {
-                roadIDs.add(next.getID());
-            }
-            if (next instanceof Refuge) {
-                refugeIDs.add(next.getID());
-            }
+
+  /**
+   * Construct a random walk starting from this agent's current location to a
+   * random building.
+   *
+   * @return A random walk.
+   */
+  protected List<EntityID> randomWalk() {
+    List<EntityID> result = new ArrayList<EntityID>( RANDOM_WALK_LENGTH );
+    Set<EntityID> seen = new HashSet<EntityID>();
+    EntityID current = ( (Human) me() ).getPosition();
+    for ( int i = 0; i < RANDOM_WALK_LENGTH; ++i ) {
+      result.add( current );
+      seen.add( current );
+      List<EntityID> possible = new ArrayList<EntityID>(
+          neighbours.get( current ) );
+      Collections.shuffle( possible, random );
+      boolean found = false;
+      for ( EntityID next : possible ) {
+        if ( seen.contains( next ) ) {
+          continue;
         }
-        search = new SampleSearch(model);
-        neighbours = search.getGraph();
-        useSpeak = config.getValue(Constants.COMMUNICATION_MODEL_KEY).equals(SPEAK_COMMUNICATION_MODEL);
-        LOG.debug("Communcation model: " + config.getValue(Constants.COMMUNICATION_MODEL_KEY));
-        LOG.debug(useSpeak ? "Using speak model" : "Using say model");
+        current = next;
+        found = true;
+        break;
+      }
+      if ( !found ) {
+        // We reached a dead-end.
+        break;
+      }
     }
-
-    /**
-       Construct a random walk starting from this agent's current location to a random building.
-       @return A random walk.
-    */
-    protected List<EntityID> randomWalk() {
-        List<EntityID> result = new ArrayList<EntityID>(RANDOM_WALK_LENGTH);
-        Set<EntityID> seen = new HashSet<EntityID>();
-        EntityID current = ((Human)me()).getPosition();
-        for (int i = 0; i < RANDOM_WALK_LENGTH; ++i) {
-            result.add(current);
-            seen.add(current);
-            List<EntityID> possible = new ArrayList<EntityID>(neighbours.get(current));
-            Collections.shuffle(possible, random);
-            boolean found = false;
-            for (EntityID next : possible) {
-                if (seen.contains(next)) {
-                    continue;
-                }
-                current = next;
-                found = true;
-                break;
-            }
-            if (!found) {
-                // We reached a dead-end.
-                break;
-            }
-        }
-        return result;
-    }
+    return result;
+  }
 }
