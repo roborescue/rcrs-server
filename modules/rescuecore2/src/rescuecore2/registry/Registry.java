54a4e2f492d67b50ef565bbc089b87b60f9f4053
Ali <modaresi.mr@gmail.com>
2021-10-15 17:33:29 +0200

    Change registery to handle int urn TODO->complete backward compability

--- modules/rescuecore2/src/rescuecore2/registry/Registry.java
+++ modules/rescuecore2/src/rescuecore2/registry/Registry.java
@@ -31,13 +31,13 @@ public final class Registry {
 
 	static {
 		// Register the ControlMessageFactory
-		SYSTEM_REGISTRY
-				.registerMessageFactory(rescuecore2.messages.control.ControlMessageFactory.INSTANCE);
+		SYSTEM_REGISTRY.registerMessageFactory(
+				rescuecore2.messages.control.ControlMessageFactory.INSTANCE);
 	}
 
-	private final Map<String, EntityFactory> entityFactories;
-	private final Map<String, PropertyFactory> propertyFactories;
-	private final Map<String, MessageFactory> messageFactories;
+	private final Map<Integer, EntityFactory> entityFactories;
+	private final Map<Integer, PropertyFactory> propertyFactories;
+	private final Map<Integer, MessageFactory> messageFactories;
 
 	private final Registry parent;
 	private final String name;
@@ -53,8 +53,7 @@ public final class Registry {
 	 * Create a new Registry with a particular name that uses the system
 	 * registry as a parent.
 	 * 
-	 * @param name
-	 *            The name of this Registry.
+	 * @param name The name of this Registry.
 	 */
 	public Registry(String name) {
 		this(name, SYSTEM_REGISTRY);
@@ -63,8 +62,7 @@ public final class Registry {
 	/**
 	 * Create a new Registry with a particular parent.
 	 * 
-	 * @param parent
-	 *            The parent Registry.
+	 * @param parent The parent Registry.
 	 */
 	public Registry(Registry parent) {
 		this(null, parent);
@@ -73,17 +71,15 @@ public final class Registry {
 	/**
 	 * Create a new Registry with a particular name and parent.
 	 * 
-	 * @param name
-	 *            The name of this Registry.
-	 * @param parent
-	 *            The parent Registry.
+	 * @param name   The name of this Registry.
+	 * @param parent The parent Registry.
 	 */
 	public Registry(String name, Registry parent) {
 		this.name = name;
 		this.parent = parent;
-		entityFactories = new HashMap<String, EntityFactory>();
-		propertyFactories = new HashMap<String, PropertyFactory>();
-		messageFactories = new HashMap<String, MessageFactory>();
+		entityFactories = new HashMap<Integer, EntityFactory>();
+		propertyFactories = new HashMap<Integer, PropertyFactory>();
+		messageFactories = new HashMap<Integer, MessageFactory>();
 	}
 
 	/**
@@ -98,8 +94,7 @@ public final class Registry {
 	/**
 	 * Set the current Registry for this thread.
 	 * 
-	 * @param r
-	 *            The current Registry for this thread.
+	 * @param r The current Registry for this thread.
 	 */
 	public static void setCurrentRegistry(Registry r) {
 		CURRENT_REGISTRY.set(r);
@@ -126,11 +121,10 @@ public final class Registry {
 	 * Register an entity factory. This will register all entity URNs that the
 	 * factory knows about.
 	 * 
-	 * @param factory
-	 *            The entity factory to register.
+	 * @param factory The entity factory to register.
 	 */
 	public void registerEntityFactory(EntityFactory factory) {
-		for (String urn : factory.getKnownEntityURNs()) {
+		for (Integer urn : factory.getKnownEntityURNs()) {
 			registerEntityFactory(urn, factory);
 		}
 	}
@@ -139,13 +133,11 @@ public final class Registry {
 	 * Register an entity URN and assign an EntityFactory for constructing
 	 * instances of this type.
 	 * 
-	 * @param urn
-	 *            The urn to register.
-	 * @param factory
-	 *            The factory that is responsible for constructing entity
-	 *            instances of this type.
+	 * @param urn     The urn to register.
+	 * @param factory The factory that is responsible for constructing entity
+	 *                instances of this type.
 	 */
-	public void registerEntityFactory(String urn, EntityFactory factory) {
+	public void registerEntityFactory(Integer urn, EntityFactory factory) {
 		synchronized (entityFactories) {
 			EntityFactory old = entityFactories.get(urn);
 			if (old != null && old != factory) {
@@ -161,11 +153,10 @@ public final class Registry {
 	 * Register a property factory. This will register all property URNs that
 	 * the factory knows about.
 	 * 
-	 * @param factory
-	 *            The property factory to register.
+	 * @param factory The property factory to register.
 	 */
 	public void registerPropertyFactory(PropertyFactory factory) {
-		for (String urn : factory.getKnownPropertyURNs()) {
+		for (Integer urn : factory.getKnownPropertyURNs()) {
 			registerPropertyFactory(urn, factory);
 		}
 	}
@@ -174,13 +165,11 @@ public final class Registry {
 	 * Register a property URN and assign a PropertyFactory for constructing
 	 * instances of this type.
 	 * 
-	 * @param urn
-	 *            The urn to register.
-	 * @param factory
-	 *            The factory that is responsible for constructing property
-	 *            instances of this type.
+	 * @param urn     The urn to register.
+	 * @param factory The factory that is responsible for constructing property
+	 *                instances of this type.
 	 */
-	public void registerPropertyFactory(String urn, PropertyFactory factory) {
+	public void registerPropertyFactory(Integer urn, PropertyFactory factory) {
 		synchronized (propertyFactories) {
 			PropertyFactory old = propertyFactories.get(urn);
 			if (old != null && old != factory) {
@@ -196,11 +185,10 @@ public final class Registry {
 	 * Register a message factory. This will register all message URNs that the
 	 * factory knows about.
 	 * 
-	 * @param factory
-	 *            The message factory to register.
+	 * @param factory The message factory to register.
 	 */
 	public void registerMessageFactory(MessageFactory factory) {
-		for (String urn : factory.getKnownMessageURNs()) {
+		for (Integer urn : factory.getKnownMessageURNs()) {
 			registerMessageFactory(urn, factory);
 		}
 	}
@@ -209,13 +197,11 @@ public final class Registry {
 	 * Register a message URN and assign a MessageFactory for constructing
 	 * instances of this type.
 	 * 
-	 * @param urn
-	 *            The urn to register.
-	 * @param factory
-	 *            The factory that is responsible for constructing message
-	 *            instances of this type.
+	 * @param urn     The urn to register.
+	 * @param factory The factory that is responsible for constructing message
+	 *                instances of this type.
 	 */
-	public void registerMessageFactory(String urn, MessageFactory factory) {
+	public void registerMessageFactory(Integer urn, MessageFactory factory) {
 		synchronized (messageFactories) {
 			MessageFactory old = messageFactories.get(urn);
 			if (old != null && old != factory) {
@@ -233,13 +219,11 @@ public final class Registry {
 	 * {@link #registerEntityFactory(EntityFactory) previously registered}
 	 * EntityFactory.
 	 * 
-	 * @param urn
-	 *            The urn of the entity type to create.
-	 * @param id
-	 *            The EntityID of the Entity that will be created.
+	 * @param urn The urn of the entity type to create.
+	 * @param id  The EntityID of the Entity that will be created.
 	 * @return A new Entity object, or null if the urn is not recognised.
 	 */
-	public Entity createEntity(String urn, EntityID id) {
+	public Entity createEntity(int urn, EntityID id) {
 		EntityFactory factory = getEntityFactory(urn);
 		if (factory == null) {
 			Logger.warn(getName() + ": Entity " + urn + " not recognised.");
@@ -254,11 +238,10 @@ public final class Registry {
 	 * {@link #registerPropertyFactory(PropertyFactory) previously registered}
 	 * PropertyFactory.
 	 * 
-	 * @param urn
-	 *            The urn of the property type to create.
+	 * @param urn The urn of the property type to create.
 	 * @return A new Property object, or null if the urn is not recognised.
 	 */
-	public Property createProperty(String urn) {
+	public Property createProperty(Integer urn) {
 		PropertyFactory factory = getPropertyFactory(urn);
 		if (factory == null) {
 			Logger.warn(getName() + ": Property " + urn + " not recognised.");
@@ -273,15 +256,12 @@ public final class Registry {
 	 * {@link #registerMessageFactory(MessageFactory) previously registered}
 	 * MessageFactory.
 	 * 
-	 * @param urn
-	 *            The urn of the message type to create.
-	 * @param data
-	 *            An InputStream to read message data from.
+	 * @param urn  The urn of the message type to create.
+	 * @param data An InputStream to read message data from.
 	 * @return A new Message object, or null if the urn is not recognised.
-	 * @throws IOException
-	 *             If there is a problem decoding the message.
+	 * @throws IOException If there is a problem decoding the message.
 	 */
-	public Message createMessage(String urn, InputStream data)
+	public Message createMessage(Integer urn, InputStream data)
 			throws IOException {
 		MessageFactory factory = getMessageFactory(urn);
 		if (factory == null) {
@@ -290,8 +270,8 @@ public final class Registry {
 		}
 		return factory.makeMessage(urn, data);
 	}
-	
-	public Message createMessage(String urn, MessageProto data){
+
+	public Message createMessage(Integer urn, MessageProto data) {
 		MessageFactory factory = getMessageFactory(urn);
 		if (factory == null) {
 			Logger.warn(getName() + ": Message " + urn + " not recognised.");
@@ -299,14 +279,14 @@ public final class Registry {
 		}
 		return factory.makeMessage(urn, data);
 	}
+
 	/**
 	 * Get the entity factory for a URN, delegating to the parent if required.
 	 * 
-	 * @param urn
-	 *            The URN to look up.
+	 * @param urn The URN to look up.
 	 * @return An EntityFactory, or null if the URN is not recognised.
 	 */
-	protected EntityFactory getEntityFactory(String urn) {
+	protected EntityFactory getEntityFactory(int urn) {
 		EntityFactory result = null;
 		synchronized (entityFactories) {
 			result = entityFactories.get(urn);
@@ -320,11 +300,10 @@ public final class Registry {
 	/**
 	 * Get the property factory for a URN, delegating to the parent if required.
 	 * 
-	 * @param urn
-	 *            The URN to look up.
+	 * @param urn The URN to look up.
 	 * @return A PropertyFactory, or null if the URN is not recognised.
 	 */
-	protected PropertyFactory getPropertyFactory(String urn) {
+	protected PropertyFactory getPropertyFactory(Integer urn) {
 		PropertyFactory result = null;
 		synchronized (propertyFactories) {
 			result = propertyFactories.get(urn);
@@ -338,11 +317,10 @@ public final class Registry {
 	/**
 	 * Get the message factory for a URN, delegating to the parent if required.
 	 * 
-	 * @param urn
-	 *            The URN to look up.
+	 * @param urn The URN to look up.
 	 * @return A MessageFactory, or null if the URN is not recognised.
 	 */
-	protected MessageFactory getMessageFactory(String urn) {
+	protected MessageFactory getMessageFactory(Integer urn) {
 		MessageFactory result = null;
 		synchronized (messageFactories) {
 			result = messageFactories.get(urn);
@@ -351,5 +329,18 @@ public final class Registry {
 			result = parent.getMessageFactory(urn);
 		}
 		return result;
+	}
+
+	public static String toURN_V1(int urn) {
+		return urn + "";
+	}
+
+	public static int toURN_V2(String urn) {
+		return 0;
+	}
+
+	public static String ToPrettyName(int urn) {
+		// TODO Auto-generated method stub
+		return urn + "";
 	}
 }
