5653c314bb858e18bec93cec6fe55c0db5eb5860
Ali <modaresi.mr@gmail.com>
2021-10-15 17:40:46 +0200

    Add new AK Connect Component to use String URN for V1 and Integer URN for V2

--- modules/rescuecore2/src/rescuecore2/messages/control/AKConnect.java
+++ modules/rescuecore2/src/rescuecore2/messages/control/AKConnect.java
@@ -3,95 +3,127 @@ package rescuecore2.messages.control;
 import rescuecore2.messages.Control;
 import rescuecore2.messages.AbstractMessage;
 import rescuecore2.messages.components.IntComponent;
+import rescuecore2.messages.components.IntListComponent;
 import rescuecore2.messages.components.StringComponent;
 import rescuecore2.messages.components.StringListComponent;
 import rescuecore2.messages.protobuf.RCRSProto.MessageProto;
+import rescuecore2.registry.Registry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import java.io.InputStream;
 import java.io.IOException;
 
 /**
-   A message for connecting an agent to the kernel.
+ * A message for connecting an agent to the kernel.
  */
 public class AKConnect extends AbstractMessage implements Control {
-    private IntComponent requestID;
-    private IntComponent version;
-    private StringComponent agentName;
-    private StringListComponent requestedEntityTypes;
-
-    /**
-       An AKConnect message that populates its data from a stream.
-       @param in The InputStream to read.
-       @throws IOException If there is a problem reading the stream.
-     */
-    public AKConnect(InputStream in) throws IOException {
-        this();
-        read(in);
-    }
-
-    /**
-       An AKConnect with particular version, requestID and requested entity types.
-       @param requestID The request ID.
-       @param version The version number.
-       @param agentName The name of the agent.
-       @param requestedEntityTypes The set of requested entity types.
-     */
-    public AKConnect(int requestID, int version, String agentName, String... requestedEntityTypes) {
-        this();
-        this.requestID.setValue(requestID);
-        this.version.setValue(version);
-        this.agentName.setValue(agentName);
-        this.requestedEntityTypes.setValues(requestedEntityTypes);
-    }
-
-    private AKConnect() {
-        super(ControlMessageURN.AK_CONNECT);
-        requestID = new IntComponent("Request ID");
-        version = new IntComponent("Version");
-        agentName = new StringComponent("Name");
-        requestedEntityTypes = new StringListComponent("Requested entity types");
-        addMessageComponent(requestID);
-        addMessageComponent(version);
-        addMessageComponent(agentName);
-        addMessageComponent(requestedEntityTypes);
-    }
-
-    public AKConnect(MessageProto proto) {
-    	this();
-    	fromMessageProto(proto);
-    }
+	private IntComponent requestID;
+	private IntComponent version;
+	private StringComponent agentName;
+	private StringListComponent requestedEntityTypes_v1;
+	private IntListComponent requestedEntityTypes;
 
 	/**
-       Get the version number of this request.
-       @return The version number.
-     */
-    public int getVersion() {
-        return version.getValue();
-    }
+	 * An AKConnect message that populates its data from a stream.
+	 * 
+	 * @param in The InputStream to read.
+	 * @throws IOException If there is a problem reading the stream.
+	 */
+	public AKConnect(InputStream in) throws IOException {
+		this();
+		read(in);
+	}
 
-    /**
-       Get the request ID.
-       @return The request ID.
-     */
-    public int getRequestID() {
-        return requestID.getValue();
-    }
+	/**
+	 * An AKConnect with particular version, requestID and requested entity
+	 * types.
+	 * 
+	 * @param requestID            The request ID.
+	 * @param version              The version number.
+	 * @param agentName            The name of the agent.
+	 * @param requestedEntityTypes The set of requested entity types.
+	 */
+	public AKConnect(int requestID, int version, String agentName,
+			int[] requestedEntityTypes) {
+		this();
+		this.requestID.setValue(requestID);
+		this.version.setValue(version);
+		this.agentName.setValue(agentName);
+		this.requestedEntityTypes.setValues(requestedEntityTypes);
+		if (version == 1) {
+			ArrayList<String> newdata = new ArrayList<>();
+			for (int re : requestedEntityTypes)
+				newdata.add(Registry.toURN_V1(re));
+			this.requestedEntityTypes_v1.setValues(newdata);
+		}
+	}
 
-    /**
-       Get the name of the agent making this request.
-       @return The agent name.
-    */
-    public String getAgentName() {
-        return agentName.getValue();
-    }
+	private AKConnect() {
+		super(ControlMessageURN.AK_CONNECT);
+		requestID = new IntComponent("Request ID");
+		version = new IntComponent("Version");
+		agentName = new StringComponent("Name");
+		requestedEntityTypes_v1 = new StringListComponent(
+				"Requested entity types v1");
+		requestedEntityTypes = new IntListComponent(
+				"Requested entity types");
+		addMessageComponent(requestID);
+		addMessageComponent(version);
+		addMessageComponent(agentName);
+		if (version.getValue() == 1)
+			addMessageComponent(requestedEntityTypes_v1);
+		else
+			addMessageComponent(requestedEntityTypes);
+	}
 
-    /**
-       Get the requested entity types.
-       @return The requested entity types.
-     */
-    public List<String> getRequestedEntityTypes() {
-        return requestedEntityTypes.getValues();
-    }
+	public AKConnect(MessageProto proto) {
+		this();
+		fromMessageProto(proto);
+	}
+
+	/**
+	 * Get the version number of this request.
+	 * 
+	 * @return The version number.
+	 */
+	public int getVersion() {
+		return version.getValue();
+	}
+
+	/**
+	 * Get the request ID.
+	 * 
+	 * @return The request ID.
+	 */
+	public int getRequestID() {
+		return requestID.getValue();
+	}
+
+	/**
+	 * Get the name of the agent making this request.
+	 * 
+	 * @return The agent name.
+	 */
+	public String getAgentName() {
+		return agentName.getValue();
+	}
+
+	/**
+	 * Get the requested entity types.
+	 * 
+	 * @return The requested entity types.
+	 */
+	public List<Integer> getRequestedEntityTypes() {
+		if (version.getValue() == 1) {
+
+			List<Integer> list = new ArrayList<>();
+			for (String str : requestedEntityTypes_v1.getValues()) {
+				list.add(Registry.toURN_V2(str));
+			}
+			return list;
+		}
+		return requestedEntityTypes.getValues();
+	}
 }
