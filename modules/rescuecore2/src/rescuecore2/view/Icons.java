a6f2defca7a92f3a724926b7a897b0404920bb67
Ali <modaresi.mr@gmail.com>
2021-10-15 09:57:47 +0200

    Fix Icon generating error

--- modules/rescuecore2/src/rescuecore2/view/Icons.java
+++ modules/rescuecore2/src/rescuecore2/view/Icons.java
@@ -1,17 +1,32 @@
 package rescuecore2.view;
 
+import java.net.URL;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+
+import rescuecore2.log.Logger;
 
 /**
    Some common icons.
 */
 public final class Icons {
     /** A tick icon. */
-    public static final Icon TICK = new ImageIcon(Icons.class.getClassLoader().getResource("rescuecore2/view/tick.png"));
+    public static final Icon TICK = Icons.get(Icons.class,"rescuecore2/view/tick.png");
 
     /** A cross icon. */
-    public static final Icon CROSS = new ImageIcon(Icons.class.getClassLoader().getResource("rescuecore2/view/cross.png"));
+    public static final Icon CROSS = Icons.get(Icons.class,"rescuecore2/view/cross.png");
 
     private Icons() {}
+    
+    public static ImageIcon get(Class<?> clas, String path) {
+    	try {
+    		URL iconPath = clas.getClassLoader().getResource(path);
+        	return new ImageIcon(iconPath);
+        	}catch(Exception e) {
+        		Logger.error(clas+" : " + path +" not find");
+        		return new ImageIcon();
+        	}
+	}
+
 }
