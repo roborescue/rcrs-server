016b2169c41691f1c1f1adc4e45e02c67acbfaa5
Keisuke ANDO <ando_k@iwate-pu.ac.jp>
2026-07-22 06:43:42 +0900

    Adjust layer colors for better visibility

--- modules/maps/src/maps/convert/osm2gml/GenerateIntersectionAreaStep.java
+++ modules/maps/src/maps/convert/osm2gml/GenerateIntersectionAreaStep.java
@@ -174,7 +174,7 @@ public class GenerateIntersectionAreaStep extends ConvertStep {
                 .title("Generate Intersection Areas")
                 .layer(LineLayer.of(map.getOSMRoads())
                         .name("OSM Roads")
-                        .color(DebugPalette.SLATE_STROKE))
+                        .color(DebugPalette.CONTEXT_STROKE))
                 .layer(PointLayer.of(map.getOSMIntersections())
                         .name("OSM Intersections")
                         .color(DebugPalette.SLATE_STROKE))
